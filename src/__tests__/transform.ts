25b69131a7113d8618b01f5a0b2f7c65bc521b6b
ikatyang <ikatyang@gmail.com>
2017-05-13 00:09:15 +0800

    Fix snapshot

--- src/__tests__/transform.ts
+++ src/__tests__/transform.ts
@@ -6,11 +6,12 @@ import {transform} from '../transform';
 
 it('should transform correctly', () => {
   const tsconfig = {some_option: 'some_value'};
-  const source_filename = path.resolve(__dirname, '../../fixtures/transform/declaration.test.ts');
+  const raw_source_filename = '../../fixtures/transform/declaration.test.ts';
+  const source_filename = path.resolve(__dirname, raw_source_filename);
   const source_text = fs.readFileSync(source_filename, 'utf8');
   expect(transform({
     tsconfig,
     source_text,
-    source_filename,
+    source_filename: raw_source_filename,
   })).toMatchSnapshot();
 });
