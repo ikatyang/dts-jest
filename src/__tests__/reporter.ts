2410bda874586d95a7f68a363c1687b8779c011a
ikatyang <ikatyang@gmail.com>
2017-08-22 18:47:27 +0800

    feat: add reporter to show current TS version

---
+++ src/__tests__/reporter.ts
@@ -0,0 +1,35 @@
+import { config_namespace } from '../definitions';
+import { Reporter } from '../reporter';
+
+const original_stdout_write = process.stdout.write;
+
+beforeEach(() => {
+  process.stdout.write = jest.fn();
+});
+
+afterAll(() => {
+  process.stdout.write = original_stdout_write;
+});
+
+it('should report correctly', () => {
+  const reporter = new Reporter();
+  reporter.onRunComplete(new Set([create_context()]));
+  expect(get_write_content()).toMatchSnapshot();
+});
+
+function create_context(): jest.Context {
+  return {
+    config: {
+      rootDir: process.cwd(),
+      globals: {},
+    },
+  } as any;
+}
+
+function get_write_content() {
+  const mocked_write: jest.MockInstance<any> = process.stdout.write as any;
+  return mocked_write.mock.calls
+    .map(x => x.join())
+    .join()
+    .replace(process.cwd(), '<cwd>');
+}
---
+++ src/reporter.ts
@@ -0,0 +1,30 @@
+import * as path from 'path';
+import { config_namespace, env_root_dir, JestConfig } from './definitions';
+import { get_config } from './utils/get-config';
+
+export class Reporter implements jest.Reporter {
+  // tslint:disable-next-line:naming-convention prefer-function-over-method
+  public onRunComplete(contexts: Set<jest.Context>) {
+    process.stdout.write('\n');
+
+    contexts.forEach(context => {
+      const {
+        typescript: { version: ts_version },
+        typescript_path: ts_path,
+      } = get_config(
+        (context.config as JestConfig).globals[config_namespace],
+        context.config.rootDir,
+      );
+
+      // istanbul ignore next
+      const context_name =
+        contexts.size > 1 ? ` ${path.basename(context.config.rootDir)}` : '';
+
+      process.stdout.write(
+        `[dts-jest]${context_name} using TypeScript v${ts_version} from ${ts_path}\n`,
+      );
+    });
+
+    process.stdout.write('\n');
+  }
+}
