96685fd43cf06b5a6c42751ef2018d6f738c65b8
ikatyang <ikatyang@gmail.com>
2017-08-22 15:20:00 +0800

    refactor: fix definition conflict

--- src/utils/for-each-comment.ts
+++ src/utils/for-each-comment.ts
@@ -1,8 +1,8 @@
 import { for_each, ForEachCallback } from 'ts-comment';
 import * as _ts from 'typescript';
 
-export const for_each_comment = for_each as (
+export const for_each_comment: (
   source: string | _ts.SourceFile,
   callback: ForEachCallback,
   ts: typeof _ts,
-) => void;
+) => void = for_each as any;
