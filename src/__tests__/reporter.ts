bf4ee48390ff2cf2ceecaf8ef74d5883d9b3b4df
Ika <ikatyang@gmail.com>
2017-08-22 19:23:48 +0800

    feat: add reporter to show current TS version (#51)
    * refactor: replace utils/root-dir with utils/env-variable
    
    * test: fix missing root-dir
    
    * feat: add reporter to show current TS version
    
    * test: fix snapshot
    
    * refactor: fix linting

---
+++ src/__tests__/reporter.ts
@@ -0,0 +1,35 @@
+import { Reporter } from '../reporter';
+
+// tslint:disable-next-line:no-unbound-method
+const original_stdout_write = process.stdout.write;
+let mocked_stdout_write: jest.MockInstance<any>;
+
+beforeEach(() => {
+  mocked_stdout_write = process.stdout.write = jest.fn();
+});
+
+afterAll(() => {
+  process.stdout.write = original_stdout_write;
+});
+
+it('should report correctly', () => {
+  const reporter = new Reporter();
+  reporter.onRunComplete(new Set([create_context()]));
+  expect(get_write_content()).toMatchSnapshot();
+});
+
+function create_context(): jest.Context {
+  return {
+    config: {
+      rootDir: process.cwd(),
+      globals: {},
+    },
+  } as any;
+}
+
+function get_write_content() {
+  return mocked_stdout_write.mock.calls
+    .map(x => x.join(''))
+    .join('')
+    .replace(process.cwd(), '<cwd>');
+}
--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -1,6 +1,12 @@
 import * as path from 'path';
-import { RawConfig, Target } from '../definitions';
+import { env_root_dir, RawConfig, Target } from '../definitions';
 import { setup } from '../setup';
+import { get_env_variable, set_env_variable } from '../utils/env-variable';
+
+const original_root_dir = get_env_variable(env_root_dir);
+
+beforeAll(() => set_env_variable(env_root_dir, process.cwd()));
+afterAll(() => set_env_variable(env_root_dir, original_root_dir));
 
 const filename = path.resolve(__dirname, '../../fixtures/snapshots/example.ts');
 const targets: Target[] = [
