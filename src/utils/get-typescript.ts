634558b775816acfe5ddafab8c4fa97145190690
ikatyang <ikatyang@gmail.com>
2017-08-22 11:03:26 +0800

    refactor: fix coverage

--- src/utils/get-typescript.ts
+++ src/utils/get-typescript.ts
@@ -4,7 +4,11 @@ import { replace_root_dir } from './replace-root-dir';
 let ts: typeof _ts | undefined;
 let ts_path: string | undefined;
 
-export const get_typescript = (id = 'typescript', root_dir: string) => {
+export const get_typescript = (
+  // istanbul ignore next
+  id = 'typescript',
+  root_dir: string,
+) => {
   if (ts_path === undefined) {
     ts_path = require.resolve(replace_root_dir(id, root_dir));
   }
