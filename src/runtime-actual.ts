62dd321094a0a698c11b20f3b359b6db135f1480
ikatyang <ikatyang@gmail.com>
2017-08-23 14:48:48 +0800

    feat(actual): use pretty-format to print `//=> ?`

--- src/runtime-actual.ts
+++ src/runtime-actual.ts
@@ -1,3 +1,4 @@
+import pretty_format = require('pretty-format');
 import { runtime_indent_spaces, ActualResult } from './definitions';
 import { indent } from './utils/indent';
 import { safe_snapshot } from './utils/safe-snapshot';
@@ -15,9 +16,11 @@ export class ActualRuntime {
       result.description === undefined ? '' : `\n${result.description}\n`;
 
     const expression = indent(result.expression, runtime_indent_spaces);
-    const expected_value = indent(result.value, runtime_indent_spaces);
-    const actual_value = indent(safe_snapshot(getter), runtime_indent_spaces);
+    const actual_value = indent(
+      safe_snapshot(getter, pretty_format),
+      runtime_indent_spaces,
+    );
 
-    return `${description}\nExpected\n\n${expression}\n\nto be\n\n${expected_value}\n\nand got\n\n${actual_value}\n`;
+    return `${description}\nEvaluated\n\n${expression}\n\nto be\n\n${actual_value}\n`;
   }
 }
--- src/utils/safe-snapshot.ts
+++ src/utils/safe-snapshot.ts
@@ -1,6 +1,9 @@
-export const safe_snapshot = (getter: () => any) => {
+export const safe_snapshot = (
+  getter: () => any,
+  transformer = (value: any) => value,
+) => {
   try {
-    return getter();
+    return transformer(getter());
   } catch (error) {
     return (error as Error).message;
   }
