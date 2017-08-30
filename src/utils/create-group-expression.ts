2978437c1ed05a0f7004be3244828cd4a28ec1f5
ikatyang <ikatyang@gmail.com>
2017-08-30 14:11:54 +0800

    refactor: centralize group-description

--- src/utils/create-group-expression.ts
+++ src/utils/create-group-expression.ts
@@ -5,8 +5,6 @@ export const create_group_expression = (
   group: TriggerGroup,
   test_expression: string,
 ) => {
-  // istanbul ignore next
-  const { method, description: raw_description = '' } = group;
-  const description = JSON.stringify(raw_description);
-  return `${method}(${description}, function () { ${test_expression} })`;
+  const description = JSON.stringify(get_group_description(group));
+  return `${group.method}(${description}, function () { ${test_expression} })`;
 };
